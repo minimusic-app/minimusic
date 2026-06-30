8458231cd16c73b0244385cc3757aaddb7dd441f
Astro - Arthur <231973511+umastrodev12@users.noreply.github.com>
2026-06-30 08:09:04 -0300

    add auth stuff

--- src/minimusic/api/auth.py
+++ src/minimusic/api/auth.py
@@ -1,5 +1,9 @@
+from fastapi import FastAPI, HTTPException, status
 from pathlib import Path
 from functools import wraps
+import datetime
+import jwt
+import os
 try:
     from minimusic.config.userconfig import Config
     from minimusic.db.userdata import UserData
@@ -9,6 +13,9 @@ except ModuleNotFoundError:
     sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
     from minimusic.config.userconfig import Config
     from minimusic.db.userdata import UserData
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 
 def admin_required():
 
@@ -23,3 +30,47 @@ def admin_required():
         return decorator
 
     return wrapper
+
+def create_jwt_token(subject: str. expires: datetime.timedelta. token_type: str) -> str:
+    payload = {
+        "sub": subject,
+        "exp": expires,
+        "token_type": token_type
+    }
+
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+def refresh_acess_token(refresh_token: str):
+    payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    if payload.get("type") != "refresh":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token type for this operation."
+        )
+
+    username = payload.get("sub")
+    if username is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token without usernama identification".
+        )
+
+    new_access_token = create_jwt_token(
+            subject=username,
+            expires_delta=datetime.timedelta(minutes=15),
+            token_type="access"
+        )
+        
+        return {"access_token": new_access_token, "token_type": "bearer"}
+        
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Expired Refresh Token. Login again."
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid refresh token."
+        )
